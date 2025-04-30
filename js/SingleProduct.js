91e2c91b31d01845d08a21d4e538030fe2dec3ff
sambhav oswal <sambhavoswal23@gmail.com>
2025-04-30 11:29:48 +0530

    payment was an issue so removed it

--- js/SingleProduct.js
+++ js/SingleProduct.js
@@ -17,3 +17,20 @@ smallimg[2].onclick = function(){
 smallimg[3].onclick = function(){
     MainImg.src = smallimg[3].src;
 }
+function run()
+{
+    document.getElementById("small-img-group").innerHTML = `<div class="details">
+        <h4>Shirt Size</h4>
+        <p>Medium</p>
+
+        <h4>Shirt Color</h4>
+        <p>Multi-color</p>
+
+        <h4>Manufacturer</h4>
+        <p>Netplay Co.</p>
+
+        <h4>Additional Details</h4>
+        <p>Material: 100% Cotton</p>
+        <p>Care Instructions: Machine wash cold with like colors, tumble dry low.</p>
+    </div>`;
+}   
